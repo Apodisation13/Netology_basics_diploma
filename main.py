9d471ffb2f69040957f1ab6a75c79475f68aa417
Pavel Soloshenko <apodisation13@gmail.com>
2021-03-16 17:40:50 +0300

    очень много всего добавлено, лучше бы я делал маленькие коммиты...

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from time import sleep
 from tqdm import tqdm
+from datetime import datetime
 import json
 
 
@@ -34,11 +35,12 @@ def validate_user_id(user_id, token):
         return True, correct_id
 
 
-def download_vk_photos(user_id, token, max_photos_count=5, album_id="profile"):
+def download_vk_photos(user_id, token, album_id="profile"):
     """
     скачивает фотки пользователя user_id
     :return словарь типа ключ - количество лайков фотки: значение - ссылка на неё
     """
+    max_photos_count = int(input("Введите, сколько фоточек хотите скачать: "))
 
     params = {
         'owner_id': user_id,
@@ -75,7 +77,16 @@ def download_vk_photos(user_id, token, max_photos_count=5, album_id="profile"):
                 if like not in links_dict.keys():
                     links_dict[like] = link
                 else:
-                    links_dict[f'{like}_{photo["date"]}'] = link
+                    # print(photo['date'])
+                    normal_date = datetime.utcfromtimestamp(int(photo["date"])).strftime('%Y-%m-%d_%H-%M-%S')
+                    # print(normal_date)
+                    links_dict[f'{like}_{str(normal_date)}'] = link
+
+        with open('output_data.json', 'w') as output_file:  # json dumper - как по условию
+            output_json = {user_id: {album_id: links_dict}}
+            json.dump(output_json, output_file)
+            print(f'Данные сохранены также в файл output_data.json ')
+
         return links_dict
 
 
@@ -87,7 +98,7 @@ def yandex_uploader(user_id: str, photos_list: dict, album_id="profile"):
     закачиваем в этот альбом все файлы из словаря photos_list
     сохраняем данные в выходной файл в формате json
     """
-    if user_photos:
+    if photos_list:
 
         ya_token = input("Введите токен яндекса: ")
 
@@ -101,19 +112,44 @@ def yandex_uploader(user_id: str, photos_list: dict, album_id="profile"):
                                     params={'path': album_folder_name},
                                     headers={'Authorization': f'OAuth {ya_token}'})
 
-            if response.status_code == 201:  # если она создалась, тогда загружаем
-                print("Начинаем загрузку")
-                for key, value in tqdm(photos_list.items()):
-                    requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload',
-                                  params={'path': f'{album_folder_name}/{key}', 'url': value},
-                                  headers={'Authorization': f'OAuth {ya_token}'})
-                    sleep(1)
-                print("Загрузка завершена")
+            if response.status_code in [201, 409]:  # если она создалась, тогда загружаем
 
-                with open('output_data.json', 'w') as output_file:
-                    output_json = {user_id: {album_id: photos_list}}
-                    json.dump(output_json, output_file)
-                    print(f'Данные сохранены также в файл output_data.json ')
+                files = requests.get('https://cloud-api.yandex.net/v1/disk/resources/',
+                                     params={'path': f'user_{user_id}/{album_id}'},
+                                     headers={'Authorization': f'OAuth {ya_token}'})
+
+                # print(files.json())
+                names = []  # проверка папки на имена файлов, чтобы потом не добавлять такие же
+                if "error" not in files.json():
+                    number = files.json()['_embedded']['items']
+                    # print(len(number))
+                    for each in number:
+                        names.append(each['name'])
+                # print(names)
+
+                print("Начинаем загрузку")
+                i = 0  # счетчик количества повторяющихся файлов
+                for key, value in tqdm(photos_list.items()):
+
+                    if str(key) not in names:
+                        requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload',
+                                      params={'path': f'{album_folder_name}/{key}', 'url': value},
+                                      headers={'Authorization': f'OAuth {ya_token}'})
+                        sleep(1)
+
+                    else:
+                        print('такой уже есть')
+                        i += 1
+
+                if i == 0:
+                    print("Загрузка завершена")
+                else:
+                    print(f'Загрузка завершена, {i} не загружено, так как они уже есть')
+
+                with open('current_time_log.txt', "a+", encoding='utf-8') as f:  # лог txt с дозаписью
+                    line = f'_user_id={user_id}_album={album_id}_____загружено_{len(photos_list) - i}_фоток\n'
+                    f.write(str(datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + line))
+                    print('log_file updated')
 
             else:  # это если она не создалась, например уже есть (повторно хотим заслать альбом)
                 print(response.json()['message'])
@@ -125,19 +161,26 @@ def yandex_uploader(user_id: str, photos_list: dict, album_id="profile"):
         print("У пользователя нет фоток или он закрытый")
 
 
-with open("token_vk.txt") as f:
-    # f.readline()  # добавить эту строчку чтобы считать токен коровина, убрать чтобы считать мою
-    token = f.read().split()[0]
+def uploader():
+    with open("token_vk.txt") as f:
+        # f.readline()  # добавить эту строчку чтобы считать токен коровина, убрать чтобы считать мою
+        token = f.read().split()[0]
 
-# token = "вставьте ваш токен ВК сюда"
+    # token = "вставьте ваш токен ВК сюда"
 
-user_id = input("Введите id пользователя, откуда будем скачивать фотографии: ")
-validate_user, user_id = validate_user_id(user_id, token)
+    user_id = input("Введите id пользователя, откуда будем скачивать фотографии: ")
 
-if validate_user:
-    user_photos = download_vk_photos(user_id, token)
+    validate_user, user_id = validate_user_id(user_id, token)
 
-    # for key, value in user_photos.items():
-    #     print(key, value)
+    if validate_user:
+        user_photos = download_vk_photos(user_id, token)
 
-    yandex_uploader(user_id, user_photos)
+        # if user_photos:
+        #     for key, value in user_photos.items():
+        #         print(key, value)
+
+        yandex_uploader(user_id, user_photos)
+
+
+if __name__ == "__main__":
+    uploader()
